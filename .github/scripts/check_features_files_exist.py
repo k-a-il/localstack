3404214e5dda63015acea0753dfca13f537927a5
Anastasia Dusak <61540676+k-a-il@users.noreply.github.com>
2025-04-03 16:25:43 +0200

    added title

--- .github/scripts/check_features_files_exist.py
+++ .github/scripts/check_features_files_exist.py
@@ -37,7 +37,7 @@ def main():
     features_file_status = map_features_files_status(services_path, changed_files)
     for file_path, exists in features_file_status.items():
         if not exists:
-            sys.stdout.write(f"::error ::Service code was changed but no corresponding feature file found at {file_path}")
+            sys.stdout.write(f"::error title=Features file is missing::Service code was changed but no corresponding feature file found at {file_path}")
 
 
 if __name__ == "__main__":
